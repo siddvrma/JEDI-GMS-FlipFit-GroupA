1cccac6c1443a763464730c4f7e740b2ac81c96d
Rahul Kumar <rahul.kumar40@BLRETV-P06NX9XW.local>
2024-01-04 09:59:48 +0530

    Java Project Updated

--- GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/bean/Admin.java
+++ GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/bean/Admin.java
@@ -3,6 +3,6 @@ package com.flipkart.bean;
 public class Admin extends User {
 
     public Admin() {
-        super("0","root", "admin@flipfit.com", "123", Role.ADMIN);
+        super("0","admin", "admin@flipfit.com", "Admin@123", Role.ADMIN);
     }
 }
