8441eb9d66f6b328b905385bd587515bc65cc2a7
krahulrangi <rahul.kumar40@flipkart.com>
2024-01-05 15:37:41 +0530

    config.properties added

--- GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/utils/DBConnection.java
+++ GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/utils/DBConnection.java
@@ -1,14 +1,12 @@
 package com.flipkart.utils;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class DBConnection {
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/FlipFit";
-    static final String USER = "root";
-    static final String PASS = "Rangi@2018";
-    private static Connection singleInstance = null;
-
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -17,19 +15,54 @@ public class DBConnection {
         }
     }
 
-    public static Connection connect() throws SQLException {
-        if (singleInstance == null || singleInstance.isClosed()) {
-            System.out.println("Creating a new connection to DB....");
-            try {
-                Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
-                System.out.println("Database Connected");
-                singleInstance = connection;
-                return connection;
-            } catch (SQLException e) {
-                throw new RuntimeException("Error establishing the database connection", e);
+//    public static Connection connect() throws SQLException {
+//        if (singleInstance == null || singleInstance.isClosed()) {
+//            System.out.println("Creating a new connection to DB....");
+//            try {
+//                Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+//                System.out.println("Database Connected");
+//                singleInstance = connection;
+//                return connection;
+//            } catch (SQLException e) {
+//                throw new RuntimeException("Error establishing the database connection", e);
+//            }
+//        } else {
+//            return singleInstance;
+//        }
+//    }
+
+    public static Connection connect() {
+        Connection connection = null;
+        if (connection != null)
+            return connection;
+        else
+        {
+            try
+            {
+                Properties prop = new Properties();
+                InputStream inputStream = DBConnection.class.getClassLoader().getResourceAsStream("./config.properties");
+                prop.load(inputStream);
+                String driver = prop.getProperty("dbdriver");
+                String url = prop.getProperty("dburl");
+                String user = prop.getProperty("dbuser");
+                String password = prop.getProperty("dbpassword");
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+
             }
-        } else {
-            return singleInstance;
+            catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return connection;
         }
+
     }
 }
