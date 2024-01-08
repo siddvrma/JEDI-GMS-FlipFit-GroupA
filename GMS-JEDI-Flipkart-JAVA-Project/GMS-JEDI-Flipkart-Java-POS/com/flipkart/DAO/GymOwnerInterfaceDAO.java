adb7169f26cc2e43f0d128321f6b72cd687eadba
Achintya Nigam <achintya.nigam@BLRETV-7TJRHQ4.local>
2024-01-08 11:43:31 +0530

    Javadoc changes

--- GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/DAO/GymOwnerInterfaceDAO.java
+++ GMS-JEDI-Flipkart-JAVA-Project/GMS-JEDI-Flipkart-Java-POS/com/flipkart/DAO/GymOwnerInterfaceDAO.java
@@ -4,14 +4,58 @@ import com.flipkart.bean.GymOwner;
 
 import java.util.List;
 
+/**
+ * This interface defines the data access operations related to Gym Owner functionality.
+ * It includes methods for retrieving gym owner lists, registering new gym owners,
+ * managing approval status, and handling approval requests.
+ */
 public interface GymOwnerInterfaceDAO {
 
-    public List<GymOwner> getGymOwnerList();
-    public void setGymOwnerList(List<GymOwner> gymOwnerList);
-    public void registerGymOwner(GymOwner gymOwner);
-    public List<GymOwner> getPendingGymOwnerList();
-    public void sendOwnerApprovalRequest(String gymOwnerId);
-    public void setPendingGymOwnerList();
-    public void validateGymOwner(String gymOwnerId, int isApproved);
+    /**
+     * Retrieves a list of all registered gym owners.
+     *
+     * @return List of gym owners
+     */
+    List<GymOwner> getGymOwnerList();
 
+    /**
+     * Sets the list of gym owners.
+     *
+     * @param gymOwnerList The list of gym owners to be set
+     */
+    void setGymOwnerList(List<GymOwner> gymOwnerList);
+
+    /**
+     * Registers a new gym owner.
+     *
+     * @param gymOwner The GymOwner object representing the details of the new gym owner
+     */
+    void registerGymOwner(GymOwner gymOwner);
+
+    /**
+     * Retrieves a list of pending gym owners awaiting approval.
+     *
+     * @return List of pending gym owners
+     */
+    List<GymOwner> getPendingGymOwnerList();
+
+    /**
+     * Sends a request for approval for a specific gym owner.
+     *
+     * @param gymOwnerId The ID of the gym owner for which the approval request is sent
+     */
+    void sendOwnerApprovalRequest(String gymOwnerId);
+
+    /**
+     * Sets the list of pending gym owners.
+     */
+    void setPendingGymOwnerList();
+
+    /**
+     * Validates a gym owner based on the provided owner ID and approval status.
+     *
+     * @param gymOwnerId The ID of the gym owner to be validated
+     * @param isApproved  The approval status (1 for approved, 0 for not approved)
+     */
+    void validateGymOwner(String gymOwnerId, int isApproved);
 }
